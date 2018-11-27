2c9012f3ad7d9596eaa6ed78f31e833ce118fc54
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-27 16:05:05 +0700

    initial commit

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function dictionarySort(arr){
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length-i; j++) {
+      if (arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(dictionarySort(['makan','duduk','tidur','terbang']));
+console.log(dictionarySort(['anggi','angga','ani','adi']));
+console.log(dictionarySort(['creed','foo fighters','zeppelin','ayam','kudanil']));
+// module.exports = Dictionary;
