31a835e4eb5c45b482b7d78e8e2bb89bc0204bec
kangman53 <hermanboy53@gmail.com>
2018-11-27 15:27:30 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
-
-module.exports = Dictionary
+// const readline = require('readline');
+// // your code here to initialize the program and take user input
+//
+// module.exports = Dictionary
+function directorySort(arr){
+  for(var i = arr.length - 1; i >= 0; i--){
+    for(var j = 1; j <= i; j++){
+      if(arr[j-1] > arr[j]){
+        var tempSwap = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = tempSwap;
+      }
+    }
+  }
+  return arr;
+}
+var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+console.log(directorySort(arrOfWord));
+var arrOfWord = ['anggi', 'angga', 'ani', 'adi'];
+console.log(directorySort(arrOfWord));
