cee92cdacdb45fa4b199e57de174b3a15182a577
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-27 18:14:01 +0700

    submission

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,28 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+
+function Dictionary(input) {
+  var result = '';
+
+  for (var i = 0; i < input.length - 1; i++) {
+    if (input[i] > input[i + 1]) {
+      var temp = input[i];
+      input[i] = input[i + 1];
+      input[i + 1] = temp;
+      i = -1;
+    }
+  }
+  
+  for (var j = 0; j < input.length; j++) {
+    if (input[j] === input[input.length-1]) {
+      result += input[j];
+    } else {
+      result += input[j] + ',';
+    }
+  }
+  return result;
+}
+
+console.log(Dictionary(['makan', 'duduk', 'tidur', 'terbang']));  //duduk,makan,terbang,tidur
+console.log(Dictionary(['anggi', 'angga', 'ani', 'adi'])); //angga,anggi,adi,ani
 
 module.exports = Dictionary
