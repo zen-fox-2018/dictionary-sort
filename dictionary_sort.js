84e102e127d4eefb94fc665dd62b7f8a52827597
ahmad <ahmad.syukron6991@gmail.com>
2018-11-27 18:18:13 +0700

    done dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,22 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+// const readline = require('readline');
 
-module.exports = Dictionary
+function dictionary(str){
+    var unsorted = true
+    while(unsorted){
+        var unsorted =  false
+        for(var i = 0; i < str.length; i ++ ){
+            if(str[i] > str[i+1]){
+                var tampung = str[i]
+                str[i] = str[i+1]
+                str[i+1] = tampung
+                var unsorted = true
+            }
+        }
+    }
+    return str
+}
+
+console.log(dictionary(['makan','duduk','berdiri','bercerita','angga','anggi']))
+
+
+//  module.exports = Dictionary
