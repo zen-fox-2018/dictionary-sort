7ab8fd1fda846683366a62070c8da50a4c0adc24
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-27 17:25:31 +0700

    Update pakai readline

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,9 +1,3 @@
-// const readline = require('readline');
-// // your code here to initialize the program and take user input
-
-// module.exports = Dictionary
-
-
 var arrOfWord = ['makan','duduk','tidur','terbang']
 var arrOfWord2 = ['anggi', 'angga', 'ani', 'adi']
 function DictionarySort (word){
@@ -23,3 +17,35 @@ function DictionarySort (word){
 }
 console.log(DictionarySort(arrOfWord))
 console.log(DictionarySort(arrOfWord2))
+
+
+// const readline = require('readline');
+// // your code here to initialize the program and take user input
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+  
+//   console.log(DictionarySort(answer.split(' ')))
+//   rl.close();
+// });
+
+// function DictionarySort (word){
+//     var checksort = false
+//     while(!checksort){
+//         checksort=true
+//         for(var i=0; i < word.length; i++){
+//             if(word[i-1]>word[i]){
+//                 checksort=false
+//                 var temp=word[i-1]
+//                 word[i-1]=word[i]
+//                 word[i]=temp
+//             }
+//         }
+//     }
+//     return word
+// }
