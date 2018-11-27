e340e5abe3a0f1c323486ed2a5aea89e9ce475d9
patriagani <patria.gani@gmail.com>
2018-11-27 17:50:40 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+function dictionarySort(input) {
+  let array = input
+  let temp = 0
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j >=0; j--) {
+      if (array[j] < array[j-1]) {
+        temp = array[j-1]
+        array[j-1] = array[j]
+        array[j] = temp
+      }
+    }
+  }
+  return array
+}
 
-module.exports = Dictionary
+console.log(dictionarySort(['makan','duduk','tidur','terbang'])); //duduk,makan,terbang,tidur
+console.log(dictionarySort(['anggi','angga','ani','adi'])); //adi,angga,anggi,ani
+
+
+// module.exports = Dictionary
