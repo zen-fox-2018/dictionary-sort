a52ebdd9c686ffc72ad7898bd76f225c5c5838b0
kangman53 <hermanboy53@gmail.com>
2018-11-27 17:03:03 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,33 @@
-// const readline = require('readline');
-// // your code here to initialize the program and take user input
+// your code here to initialize the program and take user input
+// var input = process.argv.slice(2);
+// function directorySort(arr){
+//   for(var i = arr.length - 1; i >= 0; i--){
+//     for(var j = 1; j <= i; j++){
+//       if(arr[j-1] > arr[j]){
+//         var tempSwap = arr[j-1];
+//         arr[j-1] = arr[j];
+//         arr[j] = tempSwap;
+//       }
+//     }
+//   }
+//   return arr;
+// }
 //
+// console.log(directorySort(input));
+
 // module.exports = Dictionary
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Masukkan kalimat yang akan di sort... \n', (answer) => {
+  console.log(directorySort(answer.split(' ')));
+  rl.close();
+});
+
 function directorySort(arr){
   for(var i = arr.length - 1; i >= 0; i--){
     for(var j = 1; j <= i; j++){
@@ -14,7 +40,3 @@ function directorySort(arr){
   }
   return arr;
 }
-var arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
-console.log(directorySort(arrOfWord));
-var arrOfWord = ['anggi', 'angga', 'ani', 'adi'];
-console.log(directorySort(arrOfWord));
